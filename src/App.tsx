59d8dd52ec2573ba6472b3eacc89711f8c7c252b
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-07-27 12:51:25 +0900

    chore: hompage & 배포 스크립트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/wanted-snplab/">
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           <Router />
